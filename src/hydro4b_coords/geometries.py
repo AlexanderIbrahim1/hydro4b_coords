9205086538f76847ed18cba5078cc3cd9457dd26
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2022-11-06 21:05:25 -0500

    Add irregular tetrahedron geometry

--- src/hydro4b_coords/geometries.py
+++ src/hydro4b_coords/geometries.py
@@ -10,6 +10,8 @@ from cartesian import CartesianND
 
 
 def tetrahedron(lat_const: float) -> list[CartesianND]:
+    _check_lat_const_positive(lat_const)
+    
     return [
         lat_const * Cartesian3D(-0.5, 0.0, 0.0),
         lat_const * Cartesian3D(0.5, 0.0, 0.0),
@@ -18,9 +20,38 @@ def tetrahedron(lat_const: float) -> list[CartesianND]:
     ]
 
 
+def irregular_tetrahedron(lat_const: float) -> list[CartesianND]:
+    _check_lat_const_positive(lat_const)
+
+    # the specific angle that generates the irregular tetrahedron in question
+    irregular_tetrahedron_angle = 2.0 * math.asin(math.sqrt(2.0/3.0))
+    alpha = math.pi - irregular_tetrahedron_angle
+
+    equil_tri_height = math.sqrt(3.0/4.0)
+
+    y_pos = - equil_tri_height * math.cos(alpha)
+    z_pos =   equil_tri_height * math.sin(alpha)
+
+    return [
+        lat_const * Cartesian3D(-0.5, 0.0, 0.0),
+        lat_const * Cartesian3D(0.5, 0.0, 0.0),
+        lat_const * Cartesian3D(0.0, math.sqrt(3.0 / 4.0), 0.0),
+        lat_const * Cartesian3D(0.0, y_pos, z_pos),
+    ]
+
+
 def equilateral_triangle(lat_const: float) -> list[CartesianND]:
+    _check_lat_const_positive(lat_const)
+
     return [
         lat_const * Cartesian3D(-0.5, 0.0, 0.0),
         lat_const * Cartesian3D(0.5, 0.0, 0.0),
         lat_const * Cartesian3D(0.0, math.sqrt(3.0 / 4.0), 0.0),
     ]
+
+
+def _check_lat_const_positive(lat_const: float) -> None:
+    if lat_const <= 0.0:
+        raise ValueError(f"The lattice constant must be positive. Entered: {lat_const}")
+
+
--- tests/test_geometries.py
+++ tests/test_geometries.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 
 import pytest
 from cartesian import measure
@@ -14,3 +15,52 @@ class TestTetrahedron:
         
         for (pi, pj) in itertools.combinations(points, 2):
             assert measure.distance(pi, pj) == pytest.approx(lat_const)
+
+
+class TestEquilateralTriangle:
+    def test_equidistant(self):
+        lat_const = 1.0
+        points = geometries.equilateral_triangle(lat_const)
+        distfunc = measure.euclidean_distance
+        
+        for (pi, pj) in itertools.combinations(points, 2):
+            assert measure.distance(pi, pj) == pytest.approx(lat_const)
+
+class TestIrregularTetrahedron:
+    @pytest.mark.parametrize('lat_const', [1.0, 0.5, 2.0])
+    def test_distances(self, lat_const):
+        points = geometries.irregular_tetrahedron(lat_const)
+        distfunc = measure.euclidean_distance
+    
+        pair_distances = sorted([
+            distfunc(pi, pj)
+            for (pi, pj) in itertools.combinations(points, 2)
+        ])
+        
+        assert len(pair_distances) == 6
+        
+        # five of the pair distances are equal to the lattice constant; the sixth
+        # and longest distance is sqrt(2) * lat_const
+        for i_pair in range(5):
+            assert pair_distances[i_pair] == pytest.approx(lat_const)
+        assert pair_distances[-1] == pytest.approx(lat_const * math.sqrt(2.0))
+
+
+@pytest.mark.parametrize('lat_const', [-1.0, 0.0])
+@pytest.mark.parametrize(
+    'point_creating_function',
+    [
+        geometries.equilateral_triangle,
+        geometries.tetrahedron,
+        geometries.irregular_tetrahedron,
+    ]
+)
+def test_raises_non_positive(lat_const, point_creating_function):
+    with pytest.raises(ValueError) as exc_info:
+        point_creating_function(lat_const)
+    
+    assert (
+        f"The lattice constant must be positive. Entered: {lat_const}"
+        in str(exc_info.value)
+    )
+
