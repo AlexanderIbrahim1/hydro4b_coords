82b44da37612d5ad30bfc2d6d1d42eaca8e7c4e5
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-02-16 20:01:56 -0500

    Replace asserts with exceptions

--- src/hydro4b_coords/generate/distributions.py
+++ src/hydro4b_coords/generate/distributions.py
@@ -1,8 +1,5 @@
 """
 This module contains factory functions that create specific DiscretizedDistributions.
-
-TODO
-- replace all assertions with proper exceptions
 """
 
 import math
@@ -13,11 +10,27 @@ from hydro4b_coords.generate.discretized_distribution import DiscretizedDistribu
 def exponential_decay_distribution(
     n_terms: int, x_min: float, x_max: float, *, coeff: float, decay_rate: float
 ) -> DiscretizedDistribution:
-    assert coeff > 0.0
-    assert decay_rate > 0.0
-    assert x_max > x_min > 0.0
+    """Helper function for creating an exponential decay probability distribution."""
+    _check_positive_coeff(coeff)
+    _check_positive_decay_rate(decay_rate)
 
     def exponential_decay(x: float) -> float:
         return coeff * math.exp(-decay_rate * (x - x_min))
 
     return DiscretizedDistribution(exponential_decay, n_terms, x_min, x_max)
+
+
+def _check_positive_coeff(coeff: float) -> None:
+    if coeff <= 0.0:
+        raise ValueError(
+            "The coefficient of the exponential decay must be positive.\n"
+            f"Found: coeff = {coeff: .12f}"
+        )
+
+
+def _check_positive_decay_rate(decay_rate: float) -> None:
+    if decay_rate <= 0.0:
+        raise ValueError(
+            "The decay rate of the exponential decay must be positive.\n"
+            f"Found: decay_rate = {decay_rate: .12f}"
+        )
