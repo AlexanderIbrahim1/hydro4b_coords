89ce2438d7e82ab9bde7c0b1a16eac6778432b94
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-06-18 18:33:48 -0400

    Make SixSideLengths available from sidelength_swap

--- src/hydro4b_coords/sidelength_swap/__init__.py
+++ src/hydro4b_coords/sidelength_swap/__init__.py
@@ -1,3 +1,4 @@
+from hydro4b_coords.sidelength_swap.common_types import SixSideLengths
 from hydro4b_coords.sidelength_swap.comparison import LessThanRounded
 from hydro4b_coords.sidelength_swap.comparison import LessThanEpsilon
 
